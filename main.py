15b98e4f99faa9824f76252961315058aad51a7b
amirarsalan117 <122307820+amirarsalan117@users.noreply.github.com>
2023-01-09 20:59:49 +0000

    added while and if statment for finding loser , winer and draw

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ list_play_options = ["rock", 'paper','scissors']
 
 computer_hand = random.choice(list_play_options)
 
+player_win = False
 
 
+while not player_win:
+    player_hand = input('Rock-Paper-Scissors').lower()
+    if player_hand == computer_hand:
+        print('Tie')
+    
 
